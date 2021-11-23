4f98816d246c45320f975a9b88aa751852eb59e4
Paul234Code <fayepaul234@gmail.com>
2021-11-22 22:19:07 -0500

    troisieme commit

---
+++ GuichetApplcation/Usager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guichet
+{
+    public class Usager
+    {
+        private CompteClient client;
+        private static char[] nomUtilisateur = new char[7];
+        private char[] NIP = new char[3];
+    }
+}

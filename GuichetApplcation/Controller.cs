2d96a2918124b3beacc0bc3d1db9d207f84c4815
Paul234Code <fayepaul234@gmail.com>
2021-11-23 22:22:26 -0500

    modification controller

--- GuichetApplcation/Controller.cs
+++ GuichetApplcation/Controller.cs
@@ -15,21 +15,8 @@ namespace Guichet
             {
                 Thread.Sleep(200);
             }
-<<<<<<< HEAD
-            List<Usager> ListeClients = new List<Usager>();
-=======
-            List<CompteClient> ListeClients = new List<CompteClient>();
->>>>>>> 0c80fef9d4287ecd99adcf3a0c9619e875d55cfc
-            bool mode = true;
-            while (mode)
-            {
-                Thread.Sleep(200);
-            }
-<<<<<<< HEAD
-            System.Environment.CurrentManagedThreadId
 
-=======
->>>>>>> 0c80fef9d4287ecd99adcf3a0c9619e875d55cfc
+          
         }
     }
 }

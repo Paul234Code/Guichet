1c14bb3e04ee04302fdf3f058217589302e65dbe
Paul234Code <fayepaul234@gmail.com>
2021-11-23 21:33:19 -0500

    commit

--- GuichetApplcation/Controller.cs
+++ GuichetApplcation/Controller.cs
@@ -9,14 +9,21 @@ namespace Guichet
         static void Main(string[] args)
         {
             Guichet guichet = new Guichet();
+<<<<<<< HEAD
             List<Usager> ListeClients = new List<Usager>();
+=======
+            List<CompteClient> ListeClients = new List<CompteClient>();
+>>>>>>> 0c80fef9d4287ecd99adcf3a0c9619e875d55cfc
             bool mode = true;
             while (mode)
             {
                 Thread.Sleep(200);
             }
+<<<<<<< HEAD
             System.Environment.CurrentManagedThreadId
 
+=======
+>>>>>>> 0c80fef9d4287ecd99adcf3a0c9619e875d55cfc
         }
     }
 }

a03091f5ca97f79676e76543f8057051e9e08e51
Paul Faye <64893580+Paul234Code@users.noreply.github.com>
2021-12-05 23:22:18 -0500

    validation commit # paul

--- GuichetApplcation/Controller.cs
+++ GuichetApplcation/Controller.cs
@@ -15,28 +15,28 @@ namespace Guichet
             
            
             //classe CompteEpargne
-            CompteEpargne paulEpargne = new ("Paul Faye", 2000, EtatDuCompte.ACTIF, TypeDuCompte.Epargne);
-            CompteEpargne firdaousEpargne = new ("Firdaous El Mabrooki", 2000, EtatDuCompte.VEROUILLE, TypeDuCompte.Epargne);
-            CompteEpargne jonamEpargne = new ("Jonam Dessureault", 2000, EtatDuCompte.ACTIF, TypeDuCompte.Epargne);
-            CompteEpargne simonEpargne = new ("Simon Bugeaud",3000,EtatDuCompte.ACTIF,TypeDuCompte.Epargne);
-            CompteEpargne katiaEpargne = new ("Katia Duschenau", 4000, EtatDuCompte.ACTIF, TypeDuCompte.Epargne);
+            CompteEpargne paulEpargne = new ("Paul Faye", 2000,  TypeDuCompte.Epargne);
+            CompteEpargne firdaousEpargne = new ("Firdaous El Mabrooki", 2000,  TypeDuCompte.Epargne);
+            CompteEpargne jonamEpargne = new ("Jonam Dessureault", 2000, TypeDuCompte.Epargne);
+            CompteEpargne simonEpargne = new ("Simon Bugeaud",3000,TypeDuCompte.Epargne);
+            CompteEpargne katiaEpargne = new ("Katia Duschenau", 4000, TypeDuCompte.Epargne);
             // des comptes cheques
-            CompteCheque paulCheque = new ("Paul Faye", 8000, EtatDuCompte.ACTIF, TypeDuCompte.Cheque);
-            CompteCheque firdaousCheque = new ("Firdaous El Mabrooki", 8000, EtatDuCompte.ACTIF, TypeDuCompte.Cheque);
-            CompteCheque jonamCheque = new ("Jonam Dessureault", 8000, EtatDuCompte.ACTIF, TypeDuCompte.Cheque);
-            CompteCheque simonCheque = new ("Simon Bugeaud", 3000, EtatDuCompte.ACTIF, TypeDuCompte.Cheque);
-            CompteCheque katiaCheque = new ("Katia Duschenau", 4000, EtatDuCompte.ACTIF, TypeDuCompte.Cheque);
+            CompteCheque paulCheque = new ("Paul Faye", 8000, TypeDuCompte.Cheque);
+            CompteCheque firdaousCheque = new ("Firdaous El Mabrooki", 8000, TypeDuCompte.Cheque);
+            CompteCheque jonamCheque = new ("Jonam Dessureault", 8000, TypeDuCompte.Cheque);
+            CompteCheque simonCheque = new ("Simon Bugeaud", 3000, TypeDuCompte.Cheque);
+            CompteCheque katiaCheque = new ("Katia Duschenau", 4000, TypeDuCompte.Cheque);
             // Creation d'un service
             FournisseurService service = new("Bell")
             {
                 ListeFacture = new List<Facture>()
             };
             //Creation de 5 usagers
-            Usager paul = new (paulEpargne, paulCheque, service,"paul1988", "1234");
-            Usager jonam = new (jonamEpargne, jonamCheque, service, "jonam123", "1235");
-            Usager firdaous = new (firdaousEpargne, firdaousCheque,  service, "firdaous", "1236");
-            Usager simon = new (simonEpargne, simonCheque, service, "simonbug", "1237");
-            Usager katia = new (katiaEpargne, katiaCheque,  service, "katiadus", "1238");
+            Usager paul = new (paulEpargne, paulCheque, service,"paul1988", "1234",EtatDuCompte.ACTIF);
+            Usager jonam = new (jonamEpargne, jonamCheque, service, "jonam123", "1235", EtatDuCompte.ACTIF);
+            Usager firdaous = new (firdaousEpargne, firdaousCheque,  service, "firdaous", "1236", EtatDuCompte.ACTIF);
+            Usager simon = new (simonEpargne, simonCheque, service, "simonbug", "1237", EtatDuCompte.ACTIF);
+            Usager katia = new (katiaEpargne, katiaCheque,  service, "katiadus", "1238", EtatDuCompte.ACTIF);
             // Ajouter les 5 usagers dans la liste
             guichet.AjouterUsager(paul);
             guichet.AjouterUsager(jonam);
